5624db5d6b60fe17bad89054d29791273dce948d
Shaq 44 <Shaquan44@gmail.com>
2021-11-04 14:36:09 -0700

    Learning Javascript

--- Javascript_Projects/RotatingNav/script.js
+++ Javascript_Projects/RotatingNav/script.js
@@ -1,9 +1,20 @@
-const open = document.getElementById('open');
+/*const open = document.getElementById('open');
 const close = document.getElementById('close');
 const box = document.querySelector('.box');
 
 open.addEventListener('click',()=> box.classList.add('show-nav'));
 
 close.addEventListener('click',()=> box.classList.remove('show-nav') );
+*/
 
 
+// this piece of code uses JQuery where you can rotate the page 
+$('#open').click(function(){
+    $(".box").toggleClass("show-nav");
+      console.log("button clicked");
+  });
+
+  $('#close').click(function(){
+    $(".box").toggleClass("show-nav");
+      console.log("button clicked");
+  });
